921524cd3181378853b96d09d9eb88c315664b91
Lysander <neontotem@gmail.com>
2012-02-08 07:15:38 -0500

    updated glade files These files are kept at ~/.neontotem/dcomposer so they are not always up to date. Includes flagsfile and childrunner.sh

--- glade/childrunner.sh
+++ glade/childrunner.sh
@@ -2,12 +2,13 @@
 
 $@
 myrv=$?
-echo $myrv
+
 if [ "$myrv" = 0 ];then
-	echo "Success :" $@
+	echo "SUCCESS :" $@
+    echo "exit status :"$myrv
 else
 	echo "FAILED :" $@
-	echo exit status  $myrv
+	echo "exit status :"$myrv
 fi
 
 exit 0
