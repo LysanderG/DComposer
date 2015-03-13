1aed8ea4de31a0e8d5c496e2e244909c84ecc465
Anthony Goins <neontotem@gmail.com>
2015-03-13 18:55:28 -0400

    Stopped dirview crashing from broken links.
    Just issues an error message and keeps going.  Also switched from using
    ConfigPath to SystemPath

--- elements/src/dirview.d
+++ elements/src/dirview.d
@@ -29,7 +29,7 @@ import gio.ContentType;
 
 extern (C) string GetClassName()
 {
-	return "dirview.DIR_VIEW";
+    return "dirview.DIR_VIEW";
 }
 
 
@@ -43,146 +43,151 @@ class DIR_VIEW : ELEMENT
     ListStore   mStore;
     ComboBoxText mComboText;
     Entry       mText;
-	Label		mDirLabel;
+    Label       mDirLabel;
 
-	string		mCurrentFolder;
-	bool		mViewHidden;
+    string      mCurrentFolder;
+    bool        mViewHidden;
 
-	void GoHome()
-	{
-		if(Project.TargetType != TARGET.EMPTY) Folder = Project.Folder;
-		else Folder = expandTilde("~");
-	}
-	void GotoParent()
-	{
-		Folder = Folder.dirName();
-	}
+    void GoHome()
+    {
+        if(Project.TargetType != TARGET.EMPTY) Folder = Project.Folder;
+        else Folder = expandTilde("~");
+    }
+    void GotoParent()
+    {
+        Folder = Folder.dirName();
+    }
 
-	void GotoDocFolder()
-	{
-		if(DocMan.Current is null) return;
-		Folder = dirName(DocMan.Current.Name);
-	}
+    void GotoDocFolder()
+    {
+        if(DocMan.Current is null) return;
+        Folder = dirName(DocMan.Current.Name);
+    }
 
-	void ToggleViewHidden(ToggleToolButton tglButton)
-	{
-		mViewHidden = ! (cast(bool)tglButton.getActive());
-		if(mViewHidden == true) //view hidden
-		{
-			tglButton.setStockId("dir_view_hidden_true");
-		}
-		else
-		{
-			tglButton.setStockId("dir_view_hidden_false");
-		}
-		Refresh();
-	}
+    void ToggleViewHidden(ToggleToolButton tglButton)
+    {
+        mViewHidden = ! (cast(bool)tglButton.getActive());
+        if(mViewHidden == true) //view hidden
+        {
+            tglButton.setStockId("dir_view_hidden_true");
+        }
+        else
+        {
+            tglButton.setStockId("dir_view_hidden_false");
+        }
+        Refresh();
+    }
 
 
 
-	void Refresh()
-	{
-		mStore.clear();
-		string filterText = mText.getText();
-		if(filterText.length < 1)filterText = "*";
-		foreach(DirEntry entry; dirEntries(mCurrentFolder, filterText, SpanMode.shallow))
-		{
-			string sortname;
-			string simplename = entry.name.baseName();
-			ulong filesize = entry.size;
-			string stringsize = format("% 18s ", filesize);
-			string iconname = "gtk-missing-image";
+    void Refresh()
+    {
+        mStore.clear();
+        string filterText = mText.getText();
+        if(filterText.length < 1)filterText = "*";
+        foreach(DirEntry entry; dirEntries(mCurrentFolder, filterText, SpanMode.shallow))
+        {
+            scope(failure)
+            {
+                Log.Entry("\tDirectory error detected... continuing");
+                continue;
+            }
+            string sortname;
+            string simplename = entry.name.baseName();
+            ulong filesize = entry.size;
+            string stringsize = format("% 18s ", filesize);
+            string iconname = "gtk-missing-image";
 
-			if( (simplename.startsWith(".")) && (mViewHidden) ) continue;
-			if(entry.isDir)
-			{
-				iconname = "dir_view_folder";
-				filesize = 0UL;
-				stringsize = "(DIR)";
-				sortname = "0"~simplename;
-			}
-			if(entry.isFile)
-			{
-				auto ext = extension(simplename);
-				if( (ext == ".d") || (ext == ".di"))
-				{
-					sortname = "1"~simplename;
-					iconname = "dir_view_file_d";
-				}
-				else
-				{
-					sortname = "2"~simplename;
-					iconname = "dir_view_file";
-				}
-			}
+            if( (simplename.startsWith(".")) && (mViewHidden) ) continue;
+            if(entry.isDir)
+            {
+                iconname = "dir_view_folder";
+                filesize = 0UL;
+                stringsize = "(DIR)";
+                sortname = "0"~simplename;
+            }
+            if(entry.isFile)
+            {
+                auto ext = extension(simplename);
+                if( (ext == ".d") || (ext == ".di"))
+                {
+                    sortname = "1"~simplename;
+                    iconname = "dir_view_file_d";
+                }
+                else
+                {
+                    sortname = "2"~simplename;
+                    iconname = "dir_view_file";
+                }
+            }
 
-			TreeIter ti = new TreeIter;
-			mStore.append(ti);
-			mStore.setValue(ti, 0, iconname);
-			mStore.setValue(ti, 1, simplename);
-			mStore.setValue(ti, 2, stringsize);
-			mStore.setValue(ti, 3, cast(uint)filesize);
-			mStore.setValue(ti, 4, sortname);
+            TreeIter ti = new TreeIter;
+            mStore.append(ti);
+            mStore.setValue(ti, 0, iconname);
+            mStore.setValue(ti, 1, simplename);
+            mStore.setValue(ti, 2, stringsize);
+            mStore.setValue(ti, 3, cast(uint)filesize);
+            mStore.setValue(ti, 4, sortname);
 
-		}
+        }
+        mDirLabel.setMarkup(`<span size="larger"><b>` ~ Folder ~ `</b></span>`);
 
-		mDirLabel.setMarkup(`<span size="larger"><b>` ~ Folder ~ `</b></span>`);
-	}
+    }
 
-	void WatchViewActivated(TreePath tp, TreeViewColumn tvc, TreeView tv)
-	{
-		//get what was activated
-		TreeIter trit = new TreeIter;
-		mStore.getIter(trit, tp);
+    void WatchViewActivated(TreePath tp, TreeViewColumn tvc, TreeView tv)
+    {
+        //get what was activated
+        TreeIter trit = new TreeIter;
+        mStore.getIter(trit, tp);
 
-		//basename
-		string bname = mStore.getValueString(trit, 1);
-		//fullname
-		string fullname = buildPath(Folder, bname);
+        //basename
+        string bname = mStore.getValueString(trit, 1);
+        //fullname
+        string fullname = buildPath(Folder, bname);
 
-		//what is this selection
-		if(fullname.isDir())
-		{
-			string cleanup = Folder;
+        //what is this selection
+        if(fullname.isDir())
+        {
+            string cleanup = Folder;
 
-			scope(failure)
-			{
-				ShowMessage("Can not view Folder","Please check permission/existance of " ~ fullname);
-				Folder = cleanup;
-				return;
-			}
-			Folder = fullname;
-			return;
-		}
-		if(fullname.isFile())
-		{
-			DocMan.Open(fullname);
-			return;
-		}
+            scope(failure)
+            {
+                ShowMessage("Can not view Folder","Please check permission/existance of " ~ fullname);
+                Folder = cleanup;
+                return;
+            }
+            Folder = fullname;
+            return;
+        }
+        if(fullname.isFile())
+        {
+            DocMan.Open(fullname);
+            return;
+        }
 
-		ShowMessage("What is this thing", "DComposer can't open this file (" ~ fullname ~ ")");
-	}
+        ShowMessage("What is this thing", "DComposer can't open this file (" ~ fullname ~ ")");
+    }
 
 
 
 
     public:
 
-	string Name(){ return "Directory Viewer";}
-	string Info(){ return "File Browser with a very limited number of actions to perform.";}
-	string Version() {return "00.01";}
-	string CopyRight() {return "Anthony Goins © 2014";}
-	string License() {return "New BSD license";}
-	string[] Authors() {return ["Anthony Goins <neontotem@gmail.com>"];}
+    string Name(){ return "Directory Viewer";}
+    string Info(){ return "File Browser with a very limited number of actions to perform.";}
+    string Version() {return "00.01";}
+    string CopyRight() {return "Anthony Goins © 2014";}
+    string License() {return "New BSD license";}
+    string[] Authors() {return ["Anthony Goins <neontotem@gmail.com>"];}
 
     void Engage()
     {
-		mViewHidden = true;
+        mViewHidden = true;
 
         auto builder = new Builder;
-        builder.addFromFile(Config.GetValue("dir_view", "glade_file", ConfigPath("elements/resources/dirview.glade")));
+        builder.addFromFile(Config.GetValue("dir_view", "glade_file", SystemPath("elements/resources/dirview.glade")));
 
-		//stuff we need to manipulate or just look at
+        //stuff we need to manipulate or just look at
         mRoot = cast(Box)builder.getObject("root");
         mToolbar = cast(Toolbar)builder.getObject("toolbar1");
         mView = cast(TreeView)builder.getObject("treeview1");
@@ -191,113 +196,114 @@ class DIR_VIEW : ELEMENT
         mText = cast(Entry)builder.getObject("internal-entry");
         mDirLabel = cast(Label)builder.getObject("label2");
 
-		//setup the comboboxtext stuff
-		auto savedfilters = Config.GetArray!string("dir_view", "saved_filters", ["*.d", "*.di", "*.dpro"]);
-		foreach(filter; uniq(sort(savedfilters)))mComboText.appendText(filter);
-		mText.addOnActivate(delegate void(Entry){mComboText.prependText(mText.getText());Refresh();});
-		mText.addOnIconRelease(delegate void (GtkEntryIconPosition, GdkEvent*, Entry){mText.setText("");Refresh();});
+        //setup the comboboxtext stuff
+        auto savedfilters = Config.GetArray!string("dir_view", "saved_filters", ["*.d", "*.di", "*.dpro"]);
+        foreach(filter; uniq(sort(savedfilters)))mComboText.appendText(filter);
+        mText.addOnActivate(delegate void(Entry){mComboText.prependText(mText.getText());Refresh();});
+        mText.addOnIconRelease(delegate void (GtkEntryIconPosition, GdkEvent*, Entry){mText.setText("");Refresh();});
 
 
-		//icons, actions, unspoken holy vows
-		AddIcon("dir_view_up", ConfigPath("elements/resources/navigation-090-frame.png"));
-		AddIcon("dir_view_refresh", ConfigPath("elements/resources/arrow-circle-double-135.png"));
-		AddIcon("dir_view_home", ConfigPath("elements/resources/home.png"));
-		AddIcon("dir_view_sync", ConfigPath("elements/resources/navigation-270-frame.png"));
-		AddIcon("dir_view_hidden_true", ConfigPath("elements/resources/eye-close.png"));
-		AddIcon("dir_view_hidden_false", ConfigPath("elements/resources/eye.png"));
-		AddIcon("dir_view_folder", ConfigPath("elements/resources/folder.png"));
-		AddIcon("dir_view_file", ConfigPath("elements/resources/document.png"));
-		AddIcon("dir_view_file_d", ConfigPath("elements/resources/document-attribute-d.png"));
+        //icons, actions, unspoken holy vows
+        AddIcon("dir_view_up", SystemPath("elements/resources/navigation-090-frame.png"));
+        AddIcon("dir_view_refresh", SystemPath("elements/resources/arrow-circle-double-135.png"));
+        AddIcon("dir_view_home", SystemPath("elements/resources/home.png"));
+        AddIcon("dir_view_sync", SystemPath("elements/resources/navigation-270-frame.png"));
+        AddIcon("dir_view_hidden_true", SystemPath("elements/resources/eye-close.png"));
+        AddIcon("dir_view_hidden_false", SystemPath("elements/resources/eye.png"));
+        AddIcon("dir_view_folder", SystemPath("elements/resources/folder.png"));
+        AddIcon("dir_view_file", SystemPath("elements/resources/document.png"));
+        AddIcon("dir_view_file_d", SystemPath("elements/resources/document-attribute-d.png"));
 
-		//buttons
-		auto upButton = new ToolButton("dir_view_up");
-		upButton.setHasTooltip(true);
-		upButton.setTooltipText("Go to parent directory (../)");
+        //buttons
+        auto upButton = new ToolButton("dir_view_up");
+        upButton.setHasTooltip(true);
+        upButton.setTooltipText("Go to parent directory (../)");
 
-		auto refreshButton = new ToolButton("dir_view_refresh");
-		refreshButton.setHasTooltip(true);
-		refreshButton.setTooltipText("refresh the folder view (will not automatically refresh)");
+        auto refreshButton = new ToolButton("dir_view_refresh");
+        refreshButton.setHasTooltip(true);
+        refreshButton.setTooltipText("refresh the folder view (will not automatically refresh)");
 
-		auto homeButton = new ToolButton("dir_view_home");
-		homeButton.setHasTooltip(true);
-		homeButton.setTooltipText("Go to home folder (~/ or project root folder if project is loaded)");
+        auto homeButton = new ToolButton("dir_view_home");
+        homeButton.setHasTooltip(true);
+        homeButton.setTooltipText("Go to home folder (~/ or project root folder if project is loaded)");
 
-		auto syncButton = new ToolButton("dir_view_sync");
-		syncButton.setHasTooltip(true);
-		syncButton.setTooltipText("Go to current document's folder");
+        auto syncButton = new ToolButton("dir_view_sync");
+        syncButton.setHasTooltip(true);
+        syncButton.setTooltipText("Go to current document's folder");
 
-		auto hiddenButton = new ToggleToolButton("dir_view_hidden_true");
-		hiddenButton.setHasTooltip(true);
-		hiddenButton.setTooltipText("View (or don't view) hidden files/folders");
-
-
-		//build toolbar
-		mToolbar.add(upButton);
-		mToolbar.add(refreshButton);
-		mToolbar.add(homeButton);
-		mToolbar.add(syncButton);
-		mToolbar.add(hiddenButton);
-
-		//callbacks for buttons
-		upButton.addOnClicked(delegate void(ToolButton){GotoParent();} );
-		refreshButton.addOnClicked(delegate void(ToolButton){Refresh();});
-		homeButton.addOnClicked(delegate void(ToolButton){GoHome();});
-		syncButton.addOnClicked(delegate void(ToolButton){GotoDocFolder();});
-		hiddenButton.addOnToggled(delegate void(ToggleToolButton ttb){ToggleViewHidden(ttb);});
-
-		//lock and load the tree and its model
-		Folder = getcwd();
+        auto hiddenButton = new ToggleToolButton("dir_view_hidden_true");
+        hiddenButton.setHasTooltip(true);
+        hiddenButton.setTooltipText("View (or don't view) hidden files/folders");
 
 
-		//what to do when treeview row is activated
-		mView.addOnRowActivated (&WatchViewActivated);
+        //build toolbar
+        mToolbar.add(upButton);
+        mToolbar.add(refreshButton);
+        mToolbar.add(homeButton);
+        mToolbar.add(syncButton);
+        mToolbar.add(hiddenButton);
+
+        //callbacks for buttons
+        upButton.addOnClicked(delegate void(ToolButton){GotoParent();} );
+        refreshButton.addOnClicked(delegate void(ToolButton){Refresh();});
+        homeButton.addOnClicked(delegate void(ToolButton){GoHome();});
+        syncButton.addOnClicked(delegate void(ToolButton){GotoDocFolder();});
+        hiddenButton.addOnToggled(delegate void(ToggleToolButton ttb){ToggleViewHidden(ttb);});
+
+        //lock and load the tree and its model
+        Folder = getcwd();
+
+
+        //what to do when treeview row is activated
+        mView.addOnRowActivated (&WatchViewActivated);
 
         //mRoot.showAll();
         AddSidePage(mRoot, "File Viewer");
+        mRoot.showAll();
 
         Log.Entry("Engaged");
-	}
+    }
 
 
 
-	void Disengage()
-	{
-		//combobox crap
-		string[] filters2save;
-		TreeIter trit = new TreeIter;
-		auto treemodel = mComboText.getModel();
-		int validiter = treemodel.getIterFirst(trit);
-		while(validiter)
-		{
-			filters2save ~= treemodel.getValueString(trit, 0);
-			validiter = treemodel.iterNext(trit);
-		}
-		Config.SetArray("dir_view", "saved_filters", filters2save);
+    void Disengage()
+    {
+        //combobox crap
+        string[] filters2save;
+        TreeIter trit = new TreeIter;
+        auto treemodel = mComboText.getModel();
+        int validiter = treemodel.getIterFirst(trit);
+        while(validiter)
+        {
+            filters2save ~= treemodel.getValueString(trit, 0);
+            validiter = treemodel.iterNext(trit);
+        }
+        Config.SetArray("dir_view", "saved_filters", filters2save);
 
-		RemoveSidePage(mRoot);
-		mRoot.destroy();
-
-
-		Log.Entry("Disengaged");
-	}
-
-	void Configure(){}
+        RemoveSidePage(mRoot);
+        mRoot.destroy();
 
 
-	@property string Folder()
-	{
-		return mCurrentFolder;
-	}
-	@property void Folder(string nuFolder)
-	{
-		mCurrentFolder = nuFolder;
-		Refresh();
-	}
+        Log.Entry("Disengaged");
+    }
 
-	PREFERENCE_PAGE PreferencePage()
-	{
-		return null;
-	}
+    void Configure(){}
+
+
+    @property string Folder()
+    {
+        return mCurrentFolder;
+    }
+    @property void Folder(string nuFolder)
+    {
+        mCurrentFolder = nuFolder;
+        Refresh();
+    }
+
+    PREFERENCE_PAGE PreferencePage()
+    {
+        return null;
+    }
 }
 
 
