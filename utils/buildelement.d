5594c0c1bd8327692ab1fa24b2cacb997258e52c
Anthony Goins <neontotem@gmail.com>
2015-05-22 13:26:50 -0400

    Build elements with -debug

--- utils/buildelement.d
+++ utils/buildelement.d
@@ -9,21 +9,22 @@ import std.path;
 
 int main(string[] args)
 {
-	if (args.length != 2) return 1;
-	chdir("./elements");
+    if (args.length != 2) return 1;
+    chdir("./elements");
 
-	string[] options = ["dmd",
-						"-gc",
-						"-I../src/",
-						"-I../deps/dson",
-						"-shared",
-						"-fPIC",
-						"-defaultlib=libphobos2.so",
-						"-odelements",
-						"-of" ~ args[1].setExtension("so"),
-						"src/" ~ args[1]
-						];
-	auto rv = execute(options);
-	writeln(rv.output);
-	return rv.status;
+    string[] options = ["dmd",
+                        "-gc",
+                        "-debug",
+                        "-I../src/",
+                        "-I../deps/dson",
+                        "-shared",
+                        "-fPIC",
+                        "-defaultlib=libphobos2.so",
+                        "-odelements",
+                        "-of" ~ args[1].setExtension("so"),
+                        "src/" ~ args[1]
+                        ];
+    auto rv = execute(options);
+    writeln(rv.output);
+    return rv.status;
 }
