65968c5d39c1cf99c91f1aa29ec56b775c7da330
Anthony Goins <neontotem@gmail.com>
2015-05-28 10:48:19 -0400

    Reduce size of "file size" string in dir_view elem
    Was 9 digits and 3 units
    --> 123456789KiB
    Now its
    --> 123456K
    ??

--- elements/src/dir_view.d
+++ elements/src/dir_view.d
@@ -98,9 +98,9 @@ class DIR_VIEW : ELEMENT
             string simplename = entry.name.baseName();
             ulong filesize = entry.size;
             string stringsize;
-            if(filesize > 1_024) stringsize = format("% 12sKiB ", filesize/1024);
-            else stringsize = format("% 12sB ", filesize);
-            if(filesize > 1_048_576)stringsize = format("% 12sMiB ", filesize/1_048_576);
+            if(filesize > 1_024) stringsize = format("% 6sK", filesize/1024);
+            else stringsize = format("% 6sB", filesize);
+            if(filesize > 1_048_576)stringsize = format("% 6sM", filesize/1_048_576);
             //string stringsize = format("% 18s ", filesize);
             string iconname = "gtk-missing-image";
 
