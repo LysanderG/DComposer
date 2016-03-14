88e597971f85a9dd454fb65b8dedaa5ce27efc81
Anthony Goins <neontotem@gmail.com>
2016-03-14 10:10:53 -0400

    Simple script to build elements indicates when an error occurs.

--- elementbuilder.sh
+++ elementbuilder.sh
@@ -1,18 +1,24 @@
 cd elements/src;
 
+error_arg="No errors"
+
 if [ "$#" -eq 0 ]; then
     export xfiles=`ls -1 *.d`;
 else
     export xfiles=$@;
 fi
 
-echo $xfiles;
+echo $xfiles
 echo
-cd ../../ ;
+cd ../../
 for xes in $xfiles ;
-    do echo -n $xes;
-     ./utils/buildelement.d $xes;
-     echo "$? <<<<<<";
-     echo
+    do echo -n $xes ;
+     ./utils/buildelement.d $xes ;
+     rv="$?" ;
+     if [ "$rv" -ne 0 ]; then if [ "$error_arg" = "No errors" ]; then error_arg="First Error in $xes"; fi; fi;
+     echo "$rv <<<<<<";
+     echo ;
 done
 cp elements/*.so ~/.config/dcomposer/elements;
+
+echo $error_arg
