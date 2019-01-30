3611ba7eb6f5926af76f498169d55bd7e23cb021
Anthony Goins <neontotem@gmail.com>
2019-01-29 21:26:30 -0500

    Modify element building script.
    Now works with new directory structure.

--- utils/buildelement.d
+++ utils/buildelement.d
@@ -9,24 +9,33 @@ import std.path;
 
 int main(string[] args)
 {
-    if (args.length != 2) return 1;
-    chdir("./elements");
+    writeln(args);
+    if(args.length == 2)
+    {
+        args.length = 4;
+        args[1] = "/usr/";
+        args[2] = "/usr/";
+    }
+    string DCOMPOSER_PREFIX = args[1];
+    string GTKD_IMPORT = args[2];
+    if (args.length != 4 ) return 1;
+    chdir(buildPath(DCOMPOSER_PREFIX, "lib/dcomposer/elements/src"));
 
     string[] options = ["dmd",
                         "-g",
                         "-debug",
-                        "-I../src/",
-                        "-I../deps/dson",
-			"-I~/projects/gtkd-dmd/generated/gtkd",
-			"-I~/projects/gtkd-dmd/generated/sourceview",
-			"-I~/projects/gtkd-dmd/generated/vte",
+                        "-I" ~ buildPath(DCOMPOSER_PREFIX, "include/dcomposer"),
+                        "-I" ~ GTKD_IMPORT ~ "gtkd",
+                        "-I" ~ GTKD_IMPORT ~ "sourceview",
+                        "-I" ~ GTKD_IMPORT ~ "vte",
                         "-shared",
                         "-fPIC",
                         "-defaultlib=libphobos2.so",
                         "-odelements",
-                        "-of" ~ args[1].setExtension("so"),
-                        "src/" ~ args[1]
+                        "-of" ~ buildPath(DCOMPOSER_PREFIX, "lib/dcomposer/elements", args[3].setExtension("so")),
+                        buildPath(DCOMPOSER_PREFIX, "lib/dcomposer/elements/src", args[3]),
                         ];
+    writeln(options);
     auto rv = execute(options);
     writeln(rv.output);
     return rv.status;
