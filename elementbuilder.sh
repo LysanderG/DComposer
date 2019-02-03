ac68bbfd36264b8d42ab0e76c7d42a50cddb103a
Anthony Goins <neontotem@gmail.com>
2019-02-03 10:04:27 -0500

    Giving scripts a chance on other systems.
    One day someone may find a use for dcomposer on another computer.

--- elementbuilder.sh
+++ elementbuilder.sh
@@ -1,3 +1,6 @@
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo ${SRCDIR}
+cd ${SRCDIR}
 cd lib/dcomposer/elements/src;
 
 error_arg="No errors"
@@ -12,12 +15,17 @@ echo $xfiles
 echo
 cd ../../
 for xes in $xfiles ;
-    do echo -n $xes ;
-     ../../utils/buildelement.d /home/anthony/projects/dcomposer/ ${GTKD_IMPORT} $xes ;
+    do echo -n  ;
+     ../../utils/buildelement.d ${SRCDIR} ${GTKD_IMPORT} $xes ;
      rv="$?" ;
+     if [ "$rv" -eq 200 ]; then
+         echo "Be sure to set the GTKD_IMPORT when calling elementbuilder";
+         echo "e.g. ";
+         echo "GTKD_IMPORT=/usr/include/ ./elementbuilder my_element.d";
+         exit 200;
+     fi;
      if [ "$rv" -ne 0 ]; then if [ "$error_arg" = "No errors" ]; then error_arg="First Error in $xes"; fi; fi;
-     echo " $rv <<<<<<";
-     echo ;
+    printf "%-24s %2d\n" $xes $rv;
 done
 
 echo $error_arg
--- utils/buildelement.d
+++ utils/buildelement.d
@@ -9,7 +9,6 @@ import std.path;
 
 int main(string[] args)
 {
-    writeln(args);
     if(args.length == 2)
     {
         args.length = 4;
@@ -18,13 +17,22 @@ int main(string[] args)
     }
     string DCOMPOSER_PREFIX = args[1];
     string GTKD_IMPORT = args[2];
-    if (args.length != 4 ) return 1;
+    if (args.length != 4 )
+    {
+        writeln("USAGE: buildelement DCOMPOSER_PREFIX GTKD_IMPORTS ELEMENT_MODULE");
+        writeln("\t DCOMPOSER_PREFIX is the path to where we are building dcomposer not where it is installed");
+        writeln("\t GTKD_IMPORTS is where we can find the gtkd source files");
+        writeln("\t ELEMENT_MODULE is the name of the element source file");
+        writeln("Good luck :)");
+        return 200;
+    }
     chdir(buildPath(DCOMPOSER_PREFIX, "lib/dcomposer/elements/src"));
 
     string[] options = ["dmd",
                         "-g",
                         "-debug",
                         "-I" ~ buildPath(DCOMPOSER_PREFIX, "include/dcomposer"),
+                        "-I" ~ GTKD_IMPORT,
                         "-I" ~ GTKD_IMPORT ~ "gtkd",
                         "-I" ~ GTKD_IMPORT ~ "sourceview",
                         "-I" ~ GTKD_IMPORT ~ "vte",
@@ -35,7 +43,6 @@ int main(string[] args)
                         "-of" ~ buildPath(DCOMPOSER_PREFIX, "lib/dcomposer/elements", args[3].setExtension("so")),
                         buildPath(DCOMPOSER_PREFIX, "lib/dcomposer/elements/src", args[3]),
                         ];
-    writeln(options);
     auto rv = execute(options);
     writeln(rv.output);
     return rv.status;
