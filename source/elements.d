8ec0a375f173e442e41801cce703c8d6ff1b7f40
LysanderG <neontotem@gmail.com>
2021-06-20 23:05:11 -0400

    Working on elements.
    Aka plug ins.

--- source/elements.d
+++ source/elements.d
@@ -1,15 +1,105 @@
 module elements;
 
+import std.algorithm;
+import std.conv;
+import std.file;
+import std.getopt;
+import std.path;
+import std.range;
+import core.runtime;
+import core.sys.posix.dlfcn;
+
+import qore;
+import ui;
+
+interface ELEMENT
+{
+    void Engage();
+    void Mesh();
+    void Disengage();
+
+    void Configure();
+
+    string Name();
+    string Info();
+    string Version();
+    string License();
+    string CopyRight();
+    string Authors();
+
+    //PREFERENCE_PAGE PreferencePage();
+}
+
 void Engage(ref string[] args)
 {
+    //cmdline stuff
+    getopt(args, std.getopt.config.passThrough, "disableElements|X", &mElementsDisabled,"suppress|x", &mSuppressedElements);	
+    
+    if(mElementsDisabled) 
+    {
+        Log.Entry("Elements disabled for current session (not engaged)");
+        return;
+    }
+    
+    foreach (string regLibID; Config.GetKeys("registered_libraries"))
+    {
+        REGISTERED_LIBRARY tmpLib;
+        
+        auto values  = Config.GetArray!string("registered_libraries", regLibID);
+        tmpLib.mFile = values[0];
+        tmpLib.mID = values[1];
+        tmpLib.mVersion = values[2];
+        tmpLib.mInfo = values[3];
+        tmpLib.mEnabled = values[4].to!bool;
+        tmpLib.mBroken = values[5].to!bool;
+        tmpLib.mAuthors = values[6];
+        
+        mRegisteredElements[regLibID] =  tmpLib;
+    }
+    foreach(suppressed; mSuppressedElements)
+    {
+        if(suppressed in mRegisteredElements) mRegisteredElements[suppressed].mSuppressed = true;
+        else Log.Entry(suppressed ~ "is not a registered element");
+                
+    }
+    AcquireNewElements();
+    
+    LoadElements();
+
+    foreach (successfulElement; mElements)
+    {
+        successfulElement.Engage();
+     }
+    
+    Log.Entry("Engaged");
 }
 
 void Mesh()
 {
+    foreach (elem; mElements)elem.Mesh();
+    Log.Entry("Meshed");
 }
 
 void Disengage()
-{
+{    
+    foreach(reglib; mRegisteredElements)
+    {
+        string strEnabled = reglib.mEnabled.to!string;
+        string strBroken = reglib.mBroken.to!string;
+        Config.SetArray("registered_libraries", reglib.mID, 
+        [ 
+            reglib.mFile, reglib.mID, reglib.mVersion,
+            reglib.mInfo, strEnabled, strBroken,
+            reglib.mAuthors,
+        ]);
+    
+    }
+    foreach(lib; mRegisteredElements)
+    {
+        UnloadElement(lib.mID);   
+    }
+    Config.Save();
+    Log.Entry("Disengaged");
 }
 
 string GetCmdLineOptions()
@@ -18,4 +108,154 @@ string GetCmdLineOptions()
 	rv  ="\t-X	--disableElements\tDisable loading all elements for session.\n";
 	rv ~="\t-x	--suppress=ELEMENT\tDisable specific ELEMENT for session.\n";
 	return rv;
+}
+
+
+private:
+
+ELEMENT[string]             mElements;
+
+REGISTERED_LIBRARY[string]  mRegisteredElements;    //All the elements seen loaded or not (in config)
+string[]                    mUnknownElements;       //Elements never seen before this session
+string[]                    mBrokenElements;        //Elements marked to never load (must be broken right?)
+string[]                    mSuppressedElements;    //Elements suppressed for currents session by user
+bool                        mElementsDisabled;      //user disabled elements for current session
+
+
+
+void AcquireNewElements()
+{
+    string[] allLibsFound;
+    foreach (rd; resourceDirectories)
+    {
+        scope(failure) continue;
+        foreach(string de; dirEntries(buildPath(rd,"elements"), "*.so", SpanMode.shallow))
+        {
+            de = baseName(de);
+            allLibsFound ~= de;
+            if(de !in mRegisteredElements)mUnknownElements ~= de;
+        }
+    }    
+    if(mUnknownElements.length)
+    {
+        string msg = "The following elements have been discovered\n";
+        foreach (string unknown; mUnknownElements) msg ~= "\t" ~ unknown ~"\n";
+        msg ~= "These elements can be enabled in prefrences";
+        ShowMessage("NEW ELEMENTS DISCOVERED", msg, "Continue");
+        foreach(unRegLibrary; mUnknownElements) RegisterLibrary(unRegLibrary);
+    }
+}
+void LoadElements()
+{
+    foreach(ref reglibrary; mRegisteredElements)
+    {
+        if(reglibrary.mEnabled == false) continue;
+        if(reglibrary.mBroken == true) continue;
+        LoadElement(reglibrary);
+    }
+}
+void LoadElement(ref REGISTERED_LIBRARY ElementLibrary)
+{
+   if(ElementLibrary.mPtr !is null)
+   {
+       Log.Entry(ElementLibrary.mID ~ ": Element appears to be loaded already");
+       return;
+   }
+   
+   if(ElementLibrary.mEnabled == false)
+   {
+       Log.Entry(ElementLibrary.mID ~ ": Element has been disabled by user");
+       return;
+   }
+   if(ElementLibrary.mBroken)
+   {
+       Log.Entry(ElementLibrary.mID ~ ": Element has been marked as broken");
+       return;
+   }
+   
+   ElementLibrary.mPtr = Runtime.loadLibrary(findResource(buildPath("elements", ElementLibrary.mID)));
+   
+   if(ElementLibrary.mPtr is null) 
+   {
+       ElementLibrary.mBroken = true;
+       Log.Entry("Failed to load Element :" ~ ElementLibrary.mID);
+       ShowMessage("Error Loading Element", "Faile to load " ~ ElementLibrary.mID ~ "\nElement marked as broken");
+       return;
+   } 
+   
+   auto tmpVar = dlsym(ElementLibrary.mPtr, "GetElementName");
+   if(tmpVar is null)
+   {
+       ElementLibrary.mBroken = true;
+       ElementLibrary.mEnabled = false;
+       Log.Entry(ElementLibrary.mID ~ " does not appear to be an element(Marked as broken)");
+       ShowMessage("Broken element library", ElementLibrary.mID ~ "is not a valid element(marked as broken)");
+       
+   }
+   
+   string function() GetElementName = cast(string function())tmpVar;
+   
+   ELEMENT theElement = cast(ELEMENT)Object.factory(GetElementName());
+   if(theElement is null)
+   {
+       ElementLibrary.mBroken = true;
+       ElementLibrary.mEnabled = false;
+       Log.Entry("Failed to instantiate "~GetElementName());
+       ShowMessage("Element Error", "Failed to instantiate " ~ GetElementName());
+       return;
+   }
+   ElementLibrary.mAuthors = theElement.Authors;
+   ElementLibrary.mInfo   = theElement.Info;
+
+    string strEnabled = ElementLibrary.mEnabled.to!string;
+    string strBroken = ElementLibrary.mBroken.to!string;
+   Config.SetArray("registered_libraries", ElementLibrary.mID, 
+   [ 
+        ElementLibrary.mFile, ElementLibrary.mID, ElementLibrary.mVersion,
+        ElementLibrary.mInfo, strEnabled,strBroken,
+        ElementLibrary.mAuthors,
+   ]);
+   
+   
+   mElements[ElementLibrary.mID] = theElement;
+          
+}
+
+void UnloadElement(string elementKey)
+{
+    if(elementKey !in mElements) return;
+    mElements[elementKey].Disengage();
+    
+    rt_unloadLibrary(mRegisteredElements[elementKey].mPtr);
+    mRegisteredElements[elementKey].mPtr = null;
+}
+
+void RegisterLibrary(string libraryID)
+{
+    REGISTERED_LIBRARY tmpReg;
+    
+    tmpReg.mFile = findResource(buildPath("elements", libraryID));
+    tmpReg.mID = libraryID;
+    tmpReg.mVersion = "Unknown";
+    tmpReg.mInfo = "Unknown";
+    tmpReg.mAuthors = "Unknown";
+    tmpReg.mEnabled = false;
+    tmpReg.mBroken = false;
+    
+    Config.SetArray!(string[])("registered_libraries", libraryID,
+    [libraryID, libraryID, "Unknown", "Unknown", "false", "false", tmpReg.mAuthors]);
+}
+
+
+struct REGISTERED_LIBRARY
+{
+    string      mFile; //fullpathname
+    string      mID;
+    string      mVersion;
+    string      mInfo;
+    string      mAuthors;
+    bool        mEnabled;
+    bool        mBroken;
+    bool        mSuppressed;    
+    void       *mPtr;
 }
