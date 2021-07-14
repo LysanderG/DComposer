550a35d20076de3c5a0eb6aa9eedc7a2624f7561
LysanderG <neontotem@gmail.com>
2021-07-13 23:48:32 -0400

    Be careful not to use memory from unloaded element.
    Use .idup even for literals. silly.

--- elements/curly_indent.d
+++ elements/curly_indent.d
@@ -19,13 +19,13 @@ class CURLY_INDENT : ELEMENT
 
     void Engage()
     {
-        mCurlyClose = Config.GetValue("element","curly_indent_close", true);
+        mCurlyClose = Config.GetValue("element".idup,"curly_indent_close".idup, true);
         
         mPrefDialog = new Dialog("Curly Indent Preferences", mMainWindow, DialogFlags.MODAL,["Finished"],[ResponseType.CLOSE]);
         CheckButton cb = new CheckButton("Auto Close Curly Brace?", delegate void(CheckButton cbtn)
         {
             mCurlyClose = cbtn.getActive();
-            Config.SetValue("element","curly_indent_close", mCurlyClose);
+            Config.SetValue("element".idup,"curly_indent_close".idup, mCurlyClose);
             Configure();
         });
         cb.setActive(true);
@@ -48,7 +48,7 @@ class CURLY_INDENT : ELEMENT
 
     void Configure()
     {
-        mCurlyClose = Config.GetValue("element", "curly_indent_close", true);
+        mCurlyClose = Config.GetValue("element".idup, "curly_indent_close".idup, true);
         Log.Entry("Configure");
     }
 
@@ -65,94 +65,6 @@ class CURLY_INDENT : ELEMENT
         return mPrefDialog;
     }
     
-    /*void WatchForText_old(DOC_IF self, TextIter ti, string text)
-    {
-        if(text.length > 1) return;
-        DOCUMENT doc = cast(DOCUMENT)self;
-        TextIter startTi = ti.copy();
-        TextIter endTi = ti.copy();
-        
-        scope(exit)
-        {
-            ti = new TextIter;
-            doc.getBuffer.getIterAtMark(ti, doc.getBuffer.getMark("transmitInsert"));
-            doc.getBuffer.getIterAtMark(endTi, doc.getBuffer.getMark("transmitInsert"));            
-            doc.getBuffer.getIterAtMark(startTi, doc.getBuffer.getMark("transmitInsert"));            
-        }
-            
-            
-        
-        if (text == "\n")
-        {
-            startTi.backwardLine();                      
-            auto lastline = doc.getBuffer.getText(startTi, endTi, false);
-            lastline = lastline.stripRight();
-            if(lastline.length < 1)return;
-            if(lastline[$-1] != '{')return;
-            foreach(i; 0 .. doc.getIndentWidth())doc.getBuffer.insert(ti, " ");  
-            return;         
-        }
-        
-        TextIter OpenBracketLineStart;
-        TextIter OpenBracketLineEnd;
-        if(text == "}")
-        {
-            startTi.setLineOffset(0);
-            endTi.forwardToLineEnd();
-            auto thisline = doc.getBuffer.getText(startTi, endTi, false);
-            thisline = thisline.stripLeft();
-            assert (thisline.length > 0);
-            if(thisline[0] != '}')return;
-            if(!FindMatchingBrace(ti, OpenBracketLineStart))return;
-            
-            //find line with matching bracket
-            //OpenBracketLineStart = ti.copy();
-            //int counter = 0;
-            //while(OpenBracketLineStart.backwardChar())
-           // {
-            //    assert(OpenBracketLineStart.getChar() != 0);
-            //    dwrite(OpenBracketLineStart.getChar(), " ", counter);
-            //    if(OpenBracketLineStart.getChar() == '}') counter++;
-            //    if(OpenBracketLineStart.getChar() == '{') counter--;
-            //    if(counter == 0) break;
-            //}
-            //get the 'indentation' string oblstart and oblend
-            OpenBracketLineStart.setLineOffset(0);
-            OpenBracketLineEnd = OpenBracketLineStart.copy();
-            bool moveBack;
-            while((OpenBracketLineEnd.getChar == ' ') || (OpenBracketLineEnd.getChar == '\t'))
-            {
-                OpenBracketLineEnd.forwardChar();
-                moveBack = true;
-            }
-            if(moveBack)OpenBracketLineEnd.backwardChar(); 
-            
-            string iString = doc.getBuffer.getText(OpenBracketLineStart, OpenBracketLineEnd, true);
-            dwrite("lines? ", OpenBracketLineStart.getLine, "/",OpenBracketLineEnd.getLine);
-            dwrite ("from:",OpenBracketLineStart.getLineOffset, " to:",OpenBracketLineEnd.getLineOffset);
-            dwrite("ident size = ",iString.length);
-            //if(iString.length == 0) return;
-            //delete current
-            endTi = startTi.copy();
-            moveBack = false;
-            while((endTi.getChar() == ' ') || (endTi.getChar() == '\t'))
-            {
-                endTi.forwardChar();
-                moveBack = true;
-            }
-            if(moveBack)endTi.backwardChar();
-            if(OpenBracketLineEnd.getLineOffset() == 0) endTi.forwardChar();
-            doc.getBuffer.delete_(startTi, endTi);
-            ti = new TextIter;
-            doc.buff.getIterAtMark(ti, doc.buff.getMark("transmitInsert"));
-            if(iString.length == 0)return;
-            ti.backwardChar();
-            doc.getBuffer.insert(ti, iString);
-            dwrite("ok");
-            doc.getBuffer.getIterAtMark(OpenBracketLineEnd, doc.getBuffer.getMark("transmitInsert"));
-            doc.getBuffer.getIterAtMark(OpenBracketLineStart, doc.getBuffer.getMark("transmitInsert"));
-        }
-    }*/
     
     void WatchForText(DOC_IF docIf, TextIter ti, string text)
     {
