3f65b0f25390062419d4bd6d50d158fba57687c9
LysanderG <neontotem@gmail.com>
2021-07-28 22:06:19 -0400

    Added 'locate' and 'documentation' features
    Very much a work in project.

--- elements/dcd_tool.d
+++ elements/dcd_tool.d
@@ -9,6 +9,11 @@ import std.string;
 import qore;
 import ui;
 import elements;
+import document;
+import docman;
+import ui_docbook;
+import ddocconvert;
+
 extern(C) string GetElementName()
 {
     return "dcd_tool.DCD_TOOL";
@@ -25,19 +30,54 @@ class DCD_TOOL: ELEMENT
         
         
         Transmit.DocInsertText.connect(&WatchForInsert);
-        //Transmit.ProjectEvent.connect(&WatchForProjectImports);
+        Transmit.BufferFinishedLoading.connect(&WatchForFinishedLoading);
+        Transmit.ProjectEvent.connect(&WatchForProjectImports);
         
-        StartServer();    
+        //Goto symbol declaration;
+        GActionEntry[] actEntriesLocate = [
+            { "actionLocate", &action_locate, null, null, null},
+            { "actionDocumentation", &action_documentation, null, null, null}
+        ];
+        mMainWindow.addActionEntries(actEntriesLocate, cast(void*)this);
+        uiApplication.setAccelsForAction("win.actionLocate",["F2"]);
+        AddToolObject("locate", "Locate Symbol","Find where symbol is defined.",
+            Config.GetResource("icons","locate","resources", "spectacle.png"),"win.actionLocate");
+        mContextLocate = AddMenuPart("Locate Symbol", &LocateAction, "win.actionLocate");
+        
+        //documentation
+        mDocsScroll = new ScrolledWindow();
+        mDocsLabel = new Label("Documentation");
+        mDocsScroll.add(mDocsLabel);
+        mDocsScroll.showAll();
+        uiApplication.setAccelsForAction("win.actionDocumentation", ["F1"]);
+        AddToolObject("documentation", "Symbol Documentation", "Show Doc comments from source",
+            Config.GetResource("icons", "Documentation", "resources", "spectacle.png"), "win.actionDocumentation");
+        mContextDoc =  AddMenuPart("Documentation", &DocumentationAction, "win.actionDocumentation");
+        
+        StartServer();
+        AddExtraPane(mDocsScroll, "Documentation");
+        
+        
         Log.Entry("Engaged");
     }
     void Mesh()
     {
+        
         Log.Entry("Meshed");
     }
     void Disengage()
     {
+        
+        mMainWindow.removeAction("actionLocate");
+        mMainWindow.removeAction("actionDocumentation");
+        RemoveToolObject("locate");
+        RemoveToolObject("Documentation");
+        RemoveExtraPaneWidget(mDocsScroll);
+        RemoveMenuPart(mContextDoc);
+        RemoveMenuPart(mContextLocate);
         StopServer();
         //Transmit.ProjectEvent.disconnect(&WatchForProjectImports);
+        Transmit.BufferFinishedLoading.disconnect(&WatchForFinishedLoading);
         Transmit.DocInsertText.disconnect(&WatchForInsert);
         Log.Entry("Disengaged");
     }
@@ -84,6 +124,11 @@ class DCD_TOOL: ELEMENT
     string[]    mImportPaths;
     int         mMinChars;
     bool        mDcomposerStartedServer;
+    MENU_PARTS  mContextDoc;
+    MENU_PARTS  mContextLocate;
+    
+    ScrolledWindow mDocsScroll;
+    Label          mDocsLabel;
     
     void StopServer()
     {
@@ -102,6 +147,22 @@ class DCD_TOOL: ELEMENT
         mDcomposerStartedServer = true;
         //rv = std.process.execute([mServer, " --logLevel=error"]);
        std.process.spawnProcess([mServer, "--logLevel=error"], null, std.process.Config.detached);
+       
+    }
+    
+    void AddImportPaths(string[] ipaths)
+    {
+        foreach(imp; ipaths)
+        {
+            std.process.execute([mClient, "-I"~imp]);
+        }
+    }
+    
+    void WatchForFinishedLoading(string fullFilePath)
+    {
+        if(fullFilePath !in WaitingLoads) return;
+        GetDoc(fullFilePath).GotoByteOffset(WaitingLoads[fullFilePath]);
+        WaitingLoads.remove(fullFilePath);
     }
     
     void WatchForInsert(DOC_IF Doc, TextIter ti, string text)
@@ -130,6 +191,12 @@ class DCD_TOOL: ELEMENT
             default : return;
         }
                     
+    }
+    
+    void WatchForProjectImports(PROJECT project, PROJECT_EVENT event, string key)
+    {
+        if(key != LIST_KEYS.IMPORT_PATHS) return;
+        AddImportPaths(project.List(key));   
     }
     
     
@@ -192,10 +259,75 @@ class DCD_TOOL: ELEMENT
         
         auto rv = std.process.wait(pipes.pid);
         if(candidates.length < 1) return;
-        dwrite("pushing ", candidates);
         uiCompletion.PushCallTip(Doc, candidates);
         
     }
+
+    void LocateAction(MenuItem mi)
+    { 
+        action_locate(null,null, cast(void*)this);
+    }
     
+    void DocumentationAction(MenuItem mi)
+    {
+        action_documentation(null, null, cast(void*)this);
+    }
         
+}
+
+int[string] WaitingLoads;
+extern (C)
+{
+    void action_locate(void* simAction, void* varTarget, void* voidUserData)
+	{   
+
+    	DCD_TOOL dcd = cast (DCD_TOOL)voidUserData;
+        DOC_IF Doc = GetCurrentDoc();
+        std.process.ProcessPipes pipes = std.process.pipeProcess([dcd.mClient, "-c"~Doc.Offset().to!string, "-l"]);
+
+        pipes.stdin.write(Doc.Text);
+        pipes.stdin.flush();
+        pipes.stdin.close();
+        string[] output;
+        foreach (line; pipes.stdout.byLine) output ~= line.idup;
+        
+        std.process.wait(pipes.pid);
+        if(output.length < 1) return;
+        if(output[0] == "Not found") return;
+        string file;
+        int pos;
+        formattedRead(output[0], "%s\t%s", file, pos);
+        if(file == "stdin")file = Doc.FullName;
+        
+        if(Opened(file))
+        {
+            GetDoc(file).Goto(pos);
+            return;
+        }        
+        docman.OpenDocAt(file,0,0);
+        WaitingLoads[file] = pos;        
+	}
+	
+	void action_documentation(void* simAction, void* varTarget, void* voidUserData)
+	{       
+    	DCD_TOOL dcd = cast (DCD_TOOL)voidUserData;
+        DOC_IF Doc = GetCurrentDoc();
+        std.process.ProcessPipes pipes = std.process.pipeProcess([dcd.mClient, "-c"~Doc.Offset().to!string, "-d"]);
+
+        pipes.stdin.write(Doc.Text);
+        pipes.stdin.flush();
+        pipes.stdin.close();
+        string[] output;
+        foreach (line; pipes.stdout.byLine) output ~= myEncode(line.idup);
+        
+        string fullDocs;
+        foreach(doc; output)
+        {
+            import std.regex;
+            //auto reallines = regex(r"(?<=[^\\])(\\n)","g");
+            //fullDocs ~= Ddoc2Pango(doc.replaceAll(reallines, "\n")) ~ "\n";
+            fullDocs ~= doc ~ "\n";
+        }
+        dcd.mDocsLabel.setMarkup(fullDocs);
+	}
 }
